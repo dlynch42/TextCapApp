f35b9ce0a7a86cd643501888c4e4c8686a8c4e2c
Devin Lynch <djlynch17@gmail.com>
2022-08-10 14:01:34 -0700

    Initial Commit

---
+++ TextCapApp/TextCapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextCapAppApp.swift
+//  TextCapApp
+//
+//  Created by Devin Lynch on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TextCapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
